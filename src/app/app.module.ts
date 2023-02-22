0b699b35bd4ff514db2094ca712d144d9477a9b1
jasveerkaur <jasveerk1980@gmail.com>
2023-02-22 16:33:54 +0100

    update new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,34 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+
+import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
-
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
-import { provideFirebaseApp, initializeApp} from "@angular/fire/app";
+import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
 import {environment} from "../environments/environment";
 import {getAuth, provideAuth} from "@angular/fire/auth";
 import {getFirestore, provideFirestore} from "@angular/fire/firestore";
-import {getStorage, provideStorage} from "@angular/fire/storage";
-
-
 
 
 
 @NgModule({
   declarations: [AppComponent, SignupComponent],
-
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    provideFirebaseApp(()=>initializeApp(environment.fireconfig)),
-    provideAuth(()=>getAuth()),
-    provideFirestore(()=> getFirestore()),
-    provideStorage(()=> getStorage())
+    provideFirebaseApp(() => initializeApp(environment.fireconfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import firebase from "firebase/compat";
+import {Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "@angular/fire/auth";
+import {from} from "rxjs";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: Auth) { }
+
+  signUp(email:string, password:string){
+    return from(createUserWithEmailAndPassword(this.auth, email, password));
+  }
+
+  logout(){
+    return from(this.auth.signOut());
+  }
+  login(email:string, password:string){
+    return from(signInWithEmailAndPassword(this.auth, email, password));
+  }
+
+
 }
